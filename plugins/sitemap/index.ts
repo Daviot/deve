e09514dbfe214ef5f88813f739b26e1ecf15cf50
Daviot <daviot@live.at>
2019-05-21 21:34:56 +0200

    :construction: Change hook of sitemap plugin to get correct value

--- plugins/sitemap/index.ts
+++ plugins/sitemap/index.ts
@@ -2,12 +2,15 @@ import { Hooks } from './../../code/model/hooks';
 import { Plugin } from '../../code/model/plugin';
 
 export default class SitemapPlugin extends Plugin {
+    sites:any[] = [];
     constructor(private hooks: Hooks) {
         super();
 
-        this.hooks.set('builder:get-data#after', (data: any)=> {
-            //@todo write sitemap file
-            return data;
+        this.hooks.set('builder#after', async (files: string[])=> {
+            //console.log(files)
+            console.log('after');
+            console.log(files);
+            return files;
         });
     }
 
