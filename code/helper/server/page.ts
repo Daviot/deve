ea9db0aa2d60a14d361ce37ef981caafdca82ab1
Daviot <daviot@live.at>
2019-07-01 21:31:12 +0200

    :sparkles: create data for the pages

--- code/helper/server/page.ts
+++ code/helper/server/page.ts
@@ -14,37 +14,46 @@ export class PageController {
     }
 
     async getOneOrAll(req: any, res: any) {
-
-        if(req.originalUrl == '/api/page') {
+        if (req.originalUrl == '/api/page') {
             await this.all(req, res);
         } else {
             await this.get(req, res);
         }
     }
     async all(req: any, res: any) {
-        console.log('all')
-        this.path.getAllContentFiles().then((files: string[])=> {
-            console.log(files)
-            const pages: any[] = files.map((file: string) => {
-                return {
-                    path: file,
-                    api: `api/page/${file}`
+        const files = await this.path.getAllContentFiles();
+        const pages: any[] = await Promise.all(
+            files.map(async (path: string) => {
+                const data = await this.getPageData(path);
+                if (!data) {
+                    return null;
                 }
-            });
+                data.api = `api/page/${path}`; // direct link to the item
+                return data;
+            })
+        );
 
-            res.status(200).json(pages);
-        });
+        res.status(200).json(pages);
     }
 
     async get(req: any, res: any) {
         const path = this.builder.path.searchFile(req.path);
         if (path) {
-            res.status(200).json({
-                original: req.originalUrl,
-                path: path
-            });
+            const data = await this.getPageData(path);
+            res.status(200).json(data);
             return;
         }
         res.status(404).end('Not found');
+    }
+
+    async getPageData(path: string): Promise<any> {
+        const data = await this.builder.getData(path);
+        if (!data) {
+            return null;
+        }
+        data.path = path;
+        // print a reduced public path
+        data.destination = data.destination.replace(this.fs.cwd() + '/', '');
+        return data;
     }
 }
